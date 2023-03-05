ccc75ff5b9cbbfd3ca78f56d8fc1e0590ac3f76f
ryzhenkov.d <ryzhenkov.d@nakukop.com>
2023-03-05 14:45:32 +0200

    css for header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
 import {isWebp} from './modules/functions.js'
 
 isWebp();
+
+const burgers = [...document.getElementsByClassName('burger')];
+
+burgers.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('burger_active');
+        [...document.getElementsByClassName('menu__list')].forEach(cls => cls.classList.toggle('menu__list_active'));
+    })
+})
